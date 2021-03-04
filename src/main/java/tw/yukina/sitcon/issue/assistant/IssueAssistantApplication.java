bc72eb273a7c9447cb3d7406e983769e8ae18018
mochizuki <1p41p4jejo@gmail.com>
2021-03-04 12:53:11 +0800

    init

---
+++ src/main/java/tw/yukina/sitcon/issue/assistant/IssueAssistantApplication.java
@@ -0,0 +1,13 @@
+package tw.yukina.sitcon.issue.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/yukina/sitcon/issue/assistant/IssueAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.yukina.sitcon.issue.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
